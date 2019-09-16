6d2408009aa4b677579b189072cbdc43b91bfb83
IselDrew <andryshonochek0604@gmail.com>
2019-09-16 17:20:11 +0300

    Add Header code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
 import React from "react"
 
 function Header() {
-  return <div>HEADER</div>
+  return (
+    <div>
+      <div className='header-title'>
+        <span>Hello, I'm</span>
+        <br />
+        <span>Andrii Kyselov</span>
+        <br />
+        <span>FrontEnd Developer</span>
+        <br />
+      </div>
+      <div className='header-links'>
+
+      </div>
+    </div>
+  )
 }
 
 export default Header
