97c4565b5fc9bd618db31e15671c03bc55f960de
IselDrew <andryshonochek0604@gmail.com>
2019-09-16 17:12:45 +0300

    Add basic components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react"
+
+import Header from "./components/Header"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Experiences from "./components/Experiences"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="App">
-      My CV 
+      <Header />
+      <About />
+      <Projects />
+      <Experiences />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
