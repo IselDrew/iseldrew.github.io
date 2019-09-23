74c22230e3da29b13d371cb8d3fbd49ffa5c9f92
IselDrew <andryshonochek0604@gmail.com>
2019-09-23 23:43:20 +0300

    Get variables for map out of the function

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,47 +1,49 @@
-import React from "react"
-import { skills, degrees, languages } from "../data"
+import React from "react";
+import { skills, degrees, languages } from "../data";
+
+const skillsElements = skills.map(item => (
+  <div className="skill-line" key={item.id}>
+    <div className="skill-name">
+      <span>{item.name}</span>
+    </div>
+
+    <div className="skill-bar-container">
+      <div className="skill-bar">
+        <div style={{ width: item.percent }} className="skill-level"></div>
+      </div>
+    </div>
+  </div>
+));
+const languagesElements = languages.map(item => (
+  <div className="skill-line" key={item.id}>
+    <div className="skill-name">
+      <span>{item.name}</span>
+    </div>
+
+    <div className="skill-bar-container">
+      <div className="skill-bar">
+        <div style={{ width: item.percent }} className="skill-level"></div>
+      </div>
+    </div>
+  </div>
+));
+
+const degreesElements = degrees.map(item => (
+  <div className="universities" key={item.id}>
+    <span className="university-years">{item.years}</span>
+    <span className="university-speciality">{item.speciality}</span>
+    <span className="university-name">{item.university}</span>
+  </div>
+));
 
 function Experiences() {
-  const skillsElements = skills.map(item => (
-    <div className="skill-line" key={item.id}>
-      <div className="skill-name">
-        <span>{item.name}</span>
-      </div>
-
-      <div className="skill-bar-container">
-        <div className="skill-bar">
-          <div style={{ width: item.percent }} className="skill-level"></div>
-        </div>
-      </div>
-    </div>
-  ))
-  const languagesElements = languages.map(item => (
-    <div className="skill-line" key={item.id}>
-      <div className="skill-name">
-        <span>{item.name}</span>
-      </div>
-
-      <div className="skill-bar-container">
-        <div className="skill-bar">
-          <div style={{ width: item.percent }} className="skill-level"></div>
-        </div>
-      </div>
-    </div>
-  ))
-
-  const degreesElements = degrees.map(item => (
-    <div className="universities" key={item.id}>
-      <span className="university-years">{item.years}</span>
-      <span className="university-speciality">{item.speciality}</span>
-      <span className="university-name">{item.university}</span>
-    </div>
-  ))
   return (
     <div className="experiences">
       <span className="paragraph-title">Experiences</span>
       <div className="experiences-content">
         <div className="career">
           <span className="experiences-paragraph-title">Career</span>
+          <div>null, yet :(</div>
         </div>
         <div className="experiences-hr"></div>
         <div className="skills-education">
@@ -60,7 +62,7 @@ function Experiences() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Experiences
+export default Experiences;
