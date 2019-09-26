ff02b068d9d5ecc2192d800a5dda6200b68d3e95
Andrii <andryshonochek0604@gmail.com>
2019-09-26 19:34:02 +0300

    Add fonts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
+import "./fonts/Montserrat-Regular.otf"
+import "./fonts/Montserrat-Bold.otf"
 import "normalize.css"
 import "./styles.css"
 
