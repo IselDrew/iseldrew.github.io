2aa3a6c5721538a243460ab9e5cb03dc3de43501
Andrii <andryshonochek0604@gmail.com>
2019-09-26 21:19:39 +0300

    Updates

---
+++ precache-manifest.90ddafc50adcd994a239c73acf97dfd6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85114510a0131808b59bf0f8562ba33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "293b2cc437879806892b",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "96cabb1aa1378525b650",
+    "url": "/static/css/main.5cc393ea.chunk.css"
+  },
+  {
+    "revision": "293b2cc437879806892b",
+    "url": "/static/js/2.2c6b0bbc.chunk.js"
+  },
+  {
+    "revision": "96cabb1aa1378525b650",
+    "url": "/static/js/main.66d2eb5c.chunk.js"
+  },
+  {
+    "revision": "9a3a03b7afcdd4dc068c",
+    "url": "/static/js/runtime~main.e8d0bfc4.js"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "ea330f9a160bbd97791695d77a8f44d7",
+    "url": "/static/media/SnakeImage.ea330f9a.png"
+  },
+  {
+    "revision": "3d66ef51bf4864dbecc683e177c07a9b",
+    "url": "/static/media/WebsiteImage.3d66ef51.png"
+  }
+]);
